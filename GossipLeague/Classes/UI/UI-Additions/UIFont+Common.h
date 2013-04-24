a311fe1e059c20b58656779e909c30304b2b569c
Oriol Blanc <oriolblanc@gmail.com>
2013-04-25 00:15:47 +0200

    Added custom fonts

---
+++ GossipLeague/Classes/UI/UI-Additions/UIFont+Common.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+Common.h
+//  GossipLeague
+//
+//  Created by Oriol Blanc on 25/04/13.
+//  Copyright (c) 2013 Gossip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Common)
+
+@end
