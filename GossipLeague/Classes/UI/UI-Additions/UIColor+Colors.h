7e234e7c0099b510129ba7502a5d426e10f313ea
Oriol Blanc <oriolblanc@gmail.com>
2013-04-24 22:49:13 +0200

    Added UIColor category

---
+++ GossipLeague/Classes/UI/UI-Additions/UIColor+Colors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Colors.h
+//  GossipLeague
+//
+//  Created by Oriol Blanc on 24/04/13.
+//  Copyright (c) 2013 Gossip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Colors)
+
+@end
---
+++ GossipLeague/Classes/UI/UI-Additions/UIColor+Colors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Colors.m
+//  GossipLeague
+//
+//  Created by Oriol Blanc on 24/04/13.
+//  Copyright (c) 2013 Gossip. All rights reserved.
+//
+
+#import "UIColor+Colors.h"
+
+@implementation UIColor (Colors)
+
+@end
