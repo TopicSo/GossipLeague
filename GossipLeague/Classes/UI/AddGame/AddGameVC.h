f92e5b5697f488fc7da1a85724e6466311ff60e6
Oriol Blanc <oriolblanc@gmail.com>
2013-05-10 11:10:54 +0200

    Working on AddGame

---
+++ GossipLeague/Classes/UI/AddGame/AddGameVC.h
@@ -0,0 +1,13 @@
+//
+//  AddGameVC.h
+//  GossipLeague
+//
+//  Created by Oriol Blanc on 09/05/13.
+//  Copyright (c) 2013 Gossip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddGameVC : UIViewController
+
+@end
---
+++ GossipLeague/Classes/UI/AddGame/AddGameVC.m
@@ -0,0 +1,38 @@
+//
+//  AddGameVC.m
+//  GossipLeague
+//
+//  Created by Oriol Blanc on 09/05/13.
+//  Copyright (c) 2013 Gossip. All rights reserved.
+//
+
+#import "AddGameVC.h"
+
+@interface AddGameVC ()
+
+@end
+
+@implementation AddGameVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
